e04c8a185f9fa597ded89bfdc3f97d6e63ecbaf5
nitin.mewada1992 <nitin.mewada1992@gmail.com>
2021-06-02 18:37:12 +0530

    updated with contact model,migration and save data

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=128)
+    email=models.CharField(max_length=128)
+    mobile=models.CharField(max_length=12)
+    message=models.TextField()
+    date=models.DateField()
+
+
+    #models object title change
+    def __str__(self):
+        return self.name
+        
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 # Create your views here.
 def index(request):
     # return HttpResponse("this is home page")
@@ -12,4 +15,12 @@ def services(request):
     return render(request,'services.html')
 
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        mobile=request.POST.get('mobile')
+        message=request.POST.get('message')
+        contact=Contact(name=name,email=email,mobile=mobile,message=message,date=datetime.today())
+        contact.save()
+        messages.success(request, 'Your Message Has Been Sent..!')
     return render(request,'contact.html')
